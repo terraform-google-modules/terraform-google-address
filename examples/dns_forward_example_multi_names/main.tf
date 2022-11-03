7a35664fdc6454b337cb231798da02bf82dc6884
Awais Malik <awmalik@google.com>
2022-11-03 15:14:31 +0000

    lint fixes

--- examples/dns_forward_example_multi_names/main.tf
+++ examples/dns_forward_example_multi_names/main.tf
@@ -17,7 +17,7 @@
 locals {
   randomized_name = "cft-address-test-${random_string.suffix.result}"
   domain          = "justfortestingmultinames-${random_string.suffix.result}.local"
-  forward_zone    = "forward-example-multinames"  
+  forward_zone    = "forward-example-multinames"
 }
 
 module "address" {
@@ -28,15 +28,15 @@ module "address" {
   dns_domain       = local.domain
   dns_managed_zone = google_dns_managed_zone.forward.name
   dns_project      = var.project_id
-  names            = [
+  names = [
     "dynamically-reserved-ip-040",
   ]
-  dns_short_names  = [
+  dns_short_names = [
     "testip-041",
     "testip-042",
     "testip-043",
   ]
-  address_type     = "EXTERNAL"
+  address_type = "EXTERNAL"
 }
 
 resource "random_string" "suffix" {
