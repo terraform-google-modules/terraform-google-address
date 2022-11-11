5c0377fe22f33608ad8e78686714f38547af9e52
Akshay Bathija <69940325+akshaybathija-github@users.noreply.github.com>
2022-11-11 14:40:56 +0530

    Chore: Update Test to new Blueprint (#61)
    Co-authored-by: Akshay Bathija <akshaybathija@google.com>
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

---
+++ test/integration/dns_forward_example/dns_forward_example_test.go
@@ -0,0 +1,50 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dns_forward_example
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDnsForwardExample(t *testing.T) {
+	bpt := tft.NewTFBlueprintTest(t)
+
+	bpt.DefineVerify(func(assert *assert.Assertions) {
+		bpt.DefaultVerify(assert)
+
+		projectId := bpt.GetStringOutput("project_id")
+		dnsFqdns := terraform.OutputList(t, bpt.GetTFOptions(), "dns_fqdns")
+		forwardZone := bpt.GetStringOutput("forward_zone")
+		addresses := terraform.OutputList(t, bpt.GetTFOptions(), "addresses")
+		name := [3]string{"dynamically-reserved-ip-020", "dynamically-reserved-ip-021", "dynamically-reserved-ip-022"}
+		for index, element := range addresses {
+			op := gcloud.Runf(t, fmt.Sprintf("compute addresses list --filter='%s' --project %s", element, projectId)).Array()[0].Get("name")
+			assert.Contains(op.String(), name[index], "IP addresses Created")
+		}
+
+		for index, element := range dnsFqdns {
+			op1 := gcloud.Runf(t, fmt.Sprintf("dns record-sets list --filter='%s' --zone=%s --project %s", element, forwardZone, projectId)).Array()[0].Get("rrdatas")
+			assert.Contains(op1.String(), addresses[index], "Matches the FQDN to the correct IP address")
+		}
+	})
+
+	bpt.Test()
+}
---
+++ test/integration/ip_address_with_specific_ip/ip_address_with_specific_ip_test.go
@@ -0,0 +1,41 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ip_address_with_specific_ip
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
+	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIpAddressWithSpecificIp(t *testing.T) {
+	bpt := tft.NewTFBlueprintTest(t)
+
+	bpt.DefineVerify(func(assert *assert.Assertions) {
+		bpt.DefaultVerify(assert)
+
+		projectId := bpt.GetStringOutput("project_id")
+		address := "10.13.0.100"
+		name := "statically-reserved-ip-001"
+
+		op := gcloud.Runf(t, fmt.Sprintf("compute addresses list --filter='%s' --project %s", address, projectId)).Array()[0].Get("name")
+		assert.Contains(op.String(), name, "IP addresses Created")
+	})
+
+	bpt.Test()
+}
